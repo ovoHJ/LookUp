e64cbf472b42895cf6ef8d2a9d5b30b62eb31075
KangEunSeo <jbogry7@gmail.com>
2019-11-20 21:59:32 +0900

    Create CreateActivity, EnterActivity

---
+++ app/src/main/java/kr/hs/emirim/dana/lookup/EnterActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.dana.lookup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+}
