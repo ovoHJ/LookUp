e2722b1aa820eea0ebcee02cf752a435adc768f5
KangEunSeo <jbogry7@gmail.com>
2019-11-21 19:44:42 +0900

    add Dialog

--- app/src/main/java/kr/hs/emirim/dana/lookup/RoomActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/RoomActivity.java
@@ -1,22 +1,31 @@
 package kr.hs.emirim.dana.lookup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextPaint;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class RoomActivity extends AppCompatActivity {
-    TextView textView;
     private ListView m_olistView = null;    //여기도 ========================================================
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +61,44 @@ public class RoomActivity extends AppCompatActivity {
                         Color.parseColor("#00CDC1"),
                 }, null, Shader.TileMode.CLAMP);
         textView.getPaint().setShader(textShader);
+
+        findViewById(R.id.floatingBtn).setOnClickListener(floatingBtnClick);
     }
+
+    Button.OnClickListener floatingBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(RoomActivity.this, R.style.customDialog);
+            builder.setTitle("방을 나가시겠습니까?");
+
+            builder.setPositiveButton(" ", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getApplicationContext(), "Ok click", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setNegativeButton(" ", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getApplicationContext(), "No click", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+
+            Button yes = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            Drawable img1 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.yes);
+            img1.setBounds(0, 0, 70, 70);
+            yes.setCompoundDrawables(img1, null, null, null);
+
+            Button no = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+            Drawable img2 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.exit);
+            img2.setBounds(0, 0, 70, 70);
+            no.setCompoundDrawables(img2, null, null, null);
+        }
+    };
+
 }
