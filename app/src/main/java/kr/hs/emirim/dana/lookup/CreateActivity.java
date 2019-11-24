f182fadcb0e2a44fc7773ed9ed92ea185762ddd2
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-25 04:22:56 +0900

    test add date in firebase and remove data

--- app/src/main/java/kr/hs/emirim/dana/lookup/CreateActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/CreateActivity.java
@@ -1,7 +1,5 @@
 package kr.hs.emirim.dana.lookup;
 
-        import androidx.annotation.NonNull;
-        import androidx.annotation.Nullable;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
@@ -10,21 +8,20 @@ package kr.hs.emirim.dana.lookup;
         import com.google.firebase.database.DatabaseReference;
         import com.google.firebase.database.FirebaseDatabase;
 
-        import java.util.ArrayList;
-        import java.util.Collection;
         import java.util.HashMap;
         import java.util.Map;
-        import java.util.Set;
 
 public class CreateActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
+    String key = "";
 
     String name = "lookup_developers";
-    int code = 123458;
+    int code = 123459;
     int personnel = 0;
     String timer = null;
-    String nickname;
+    String nickname = "원예린";
+//    위 5줄은 테스트 용
     Map<String, String> member = new HashMap();
 
     @Override
@@ -37,20 +34,24 @@ public class CreateActivity extends AppCompatActivity {
         }
         member.put(nickname, "owner");
         newPost();
+        //roomActivity로 넘어가기 (key값 넘기기)
     }
 
     public void newPost(){
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        String key = mDatabase.child("groups").push().getKey();
-
-        personnel++;
+        key = mDatabase.child("groups").push().getKey();
         Group group = new Group(name, code, personnel, timer, member);
 
         Map<String, Object> postValues = group.toMap();
         Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put(key, postValues);
+        childUpdates.put("/groups/" + key, postValues);
 
         mDatabase.updateChildren(childUpdates);
     }
 
+//    public void removeGroup(){
+//        mDatabase.child("groups").child(key).removeValue();
+//        Log.d("remove test", "this is successed too");
+//    }
+//     remove test (원래 roomActiviry에 있을 메서드)
 }
