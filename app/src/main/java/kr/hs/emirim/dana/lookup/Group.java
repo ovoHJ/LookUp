0eb50247900254b66851003d6e03738f3373e26a
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-25 02:42:38 +0900

    change member type, add map of group

--- app/src/main/java/kr/hs/emirim/dana/lookup/Group.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/Group.java
@@ -1,23 +1,39 @@
 package kr.hs.emirim.dana.lookup;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Exclude;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Group {
 
     String name;
     int code;
     String timer;
-    String owner;
+    Map<String, String> member;
 
     public Group(){
         //Default constructor
     }
 
-    public Group(String name, int code, String timer, String owner){
-        name = this.name;
-        code = this.code;
-        timer = this.timer;
-        owner = this.owner;
+    public Group(String name, int code, String timer, Map<String, String> member){
+        this.name = name;
+        this.code = code;
+        this.timer = timer;
+        if (timer == null)
+            this.timer = "";
+        this.member = member;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("code", code);
+        result.put("timer", timer);
+        result.put("member", member);
+
+        return result;
     }
 }
