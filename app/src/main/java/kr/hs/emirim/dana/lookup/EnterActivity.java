53d03e839966eaa2f82604591c654544f191be82
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-26 06:02:46 +0900

    add editText, button, textView, onClickListner

--- app/src/main/java/kr/hs/emirim/dana/lookup/EnterActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/EnterActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.dana.lookup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class EnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+
+        EditText input_code = (EditText)findViewById(R.id.input_code);
+        EditText input_name = (EditText)findViewById(R.id.input_name);
+
+        // input_code.setText();
+
+        findViewById(R.id.start_bt).setOnClickListener(m_stBtnClick);
     }
+
+    //sddssddssd
+
+    Button.OnClickListener m_stBtnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(EnterActivity.this, RoomActivity.class); //강은서 방접속한 후 들어가는 엑티비티 명 넣으셈.
+            startActivity(intent);
+        }
+    };
+
 }
