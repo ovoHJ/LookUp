dfa081f7e5254b6f183b7aaa3225b8c7e6d7a7c1
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-27 00:41:07 +0900

    add enterRoom method using firebase

--- app/src/main/java/kr/hs/emirim/dana/lookup/EnterActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/EnterActivity.java
@@ -4,29 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnterActivity extends AppCompatActivity {
+
+    EditText input_code;
+    EditText input_name;
+
+    private DatabaseReference mDatabase;
+    DatabaseReference groupRef;
+
+    String code;
+    String name;
+    Map<String, Object> addMember = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
 
-        EditText input_code = (EditText)findViewById(R.id.input_code);
-        EditText input_name = (EditText)findViewById(R.id.input_name);
+        input_code = (EditText)findViewById(R.id.input_code);
+        input_name = (EditText)findViewById(R.id.input_name);
 
         // input_code.setText();
 
         findViewById(R.id.start_bt).setOnClickListener(m_stBtnClick);
     }
 
-    //sddssddssd
+    //enter room and add member
+    public void enterRoom(){
+        code = input_code.getText().toString();
+        name = input_name.getText().toString();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        groupRef = mDatabase.child("groups").child(code).child("member");
+
+        addMember.put(name, "user");
+        groupRef.updateChildren(addMember);
+    }//해당 코드가 DB 내에 있을 경우, 해당 코드의 데이터 키값을 받아서 member에 name : "user"형태로 추가
 
     Button.OnClickListener m_stBtnClick = new View.OnClickListener() {
         public void onClick(View v) {
+            enterRoom();
             Intent intent = new Intent(EnterActivity.this, RoomActivity.class); //강은서 방접속한 후 들어가는 엑티비티 명 넣으셈.
             startActivity(intent);
         }
