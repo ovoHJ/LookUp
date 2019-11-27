0d14761a065c0200ad9d5a8be2f4e1436da10d3d
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-28 06:02:26 +0900

    add out method and select list of members

--- app/src/main/java/kr/hs/emirim/dana/lookup/RoomActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/RoomActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -29,12 +30,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Comment;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RoomActivity extends AppCompatActivity {
 
@@ -44,10 +48,13 @@ public class RoomActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     DatabaseReference groupRef;
-    ChildEventListener childEventListener;
+    AlertDialog.Builder builder;
 
     String code;
     String name;
+    String own;
+    Map<String, Object> memberList = new HashMap<>();
+    ArrayList<String> namedata = new ArrayList<>();
 
 
     @Override
@@ -58,38 +65,50 @@ public class RoomActivity extends AppCompatActivity {
         Intent intent = getIntent();
         code = intent.getExtras().getString("code");
         name = intent.getExtras().getString("name");
-        System.out.println(code + ", " + name);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         groupRef = mDatabase.child("groups").child(code);
         rListView = (ListView)findViewById(R.id.nameList);
 
-//=====================================================================================
-        String[] nameData = {"강은서", "강지민", "강혜정", "원예린"};
-        int nDatCnt = 0;
-        final ArrayList<ItemData> dnameData = new ArrayList<>();
-        for (int i=0; i<nameData.length; i++){
-            ItemData nameItem = new ItemData();
-            nameItem.nameList = (i+1)+". "+nameData[i];
-            dnameData.add(nameItem);
-        }
-//=====================================================================================
-
-        ListAdapter rAdapter = new ListAdapter(dnameData);
-        rListView.setAdapter(rAdapter);
-
-        rArray.add(new ItemData());
-
-        rTextView = (TextView) findViewById(R.id.connectionCount);
-        rTextView.setText(rAdapter.getCount()+"명");
-
-        rListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        showList(new RoomActivity.MyCallback() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(RoomActivity.this, dnameData.get(i).getNameList(), Toast.LENGTH_SHORT).show();
+            public void onCallback(Map<String, Object> List) {
+                for (String key: List.keySet()) {
+                    if(name.equals(key)){
+                        own = List.get(key).toString();
+                        Log.d("own", own);
+                    }
+                    namedata.add(key);
+                }
+                memberList = (HashMap<String, Object>) List;
+
+                int nDatCnt = 0;
+                final ArrayList<ItemData> dnameData = new ArrayList<>();
+                for (int i=0; i< namedata.size(); i++){
+                    ItemData nameItem = new ItemData();
+                    nameItem.nameList = (i+1)+". "+ namedata.get(i);
+                    dnameData.add(nameItem);
+                }
+
+                ListAdapter rAdapter = new ListAdapter(dnameData);
+                rListView.setAdapter(rAdapter);
+
+                rArray.add(new ItemData());
+
+                rTextView = (TextView) findViewById(R.id.connectionCount);
+                rTextView.setText(rAdapter.getCount()+"명");
+
+                rListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        //선택한 리스트값이 name과 같을 경우에만
+                        outOfRoom();
+                    }
+                });
+//=====================================================================================
             }
         });
-//=====================================================================================
+
 
         rTextView = (TextView) findViewById(R.id.roomPwd);
         rTextView.setText(code);
@@ -110,76 +129,74 @@ public class RoomActivity extends AppCompatActivity {
     Button.OnClickListener floatingBtnClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(RoomActivity.this, R.style.customDialog);
-            builder.setTitle("방을 나가시겠습니까?");
-
-            builder.setPositiveButton(" ", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    groupRef.removeValue();
-                    Intent intent = new Intent(RoomActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // 액티비티 스택에 쌓인 액티비티 제거
-                    intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); //
-                    startActivity(intent);
-                    finish();
-                }
-            });
-
-            builder.setNegativeButton(" ", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) { }
-            });
-
-            AlertDialog alertDialog = builder.create();
-            alertDialog.setCanceledOnTouchOutside(false);
-            alertDialog.show();
-
-            Button yes = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
-            Drawable img1 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.yes);
-            img1.setBounds(0, 0, 70, 70);
-            yes.setCompoundDrawables(img1, null, null, null);
-
-            Button no = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
-            Drawable img2 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.exit);
-            img2.setBounds(0, 0, 70, 70);
-            no.setCompoundDrawables(img2, null, null, null);
+            Log.d("name", name);
+            System.out.println("won" + own);
+            outOfRoom();
         }
     };
 
-    public void showList(){
-        childEventListener = new ChildEventListener() {
+    public void outOfRoom(){
+        builder = new AlertDialog.Builder(RoomActivity.this, R.style.customDialog);
+        builder.setTitle("방을 나가시겠습니까?");
+
+        builder.setPositiveButton(" ", new DialogInterface.OnClickListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Group group = dataSnapshot.getValue(Group.class);
-                Log.d("member", group.name);
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(own.equals("owner")) {
+                    groupRef.removeValue();
+                } else {
+                    groupRef.child("member").child(name).removeValue();
+                }
+                Intent intent = new Intent(RoomActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // 액티비티 스택에 쌓인 액티비티 제거
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); //
+                startActivity(intent);
+                finish();
             }
+        });
 
+        builder.setNegativeButton(" ", new DialogInterface.OnClickListener() {
             @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            public void onClick(DialogInterface dialogInterface, int i) { }
+        });
 
-            }
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
 
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+        Button yes = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        Drawable img1 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.yes);
+        img1.setBounds(0, 0, 70, 70);
+        yes.setCompoundDrawables(img1, null, null, null);
 
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-        groupRef.addChildEventListener(childEventListener);
+        Button no = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        Drawable img2 = ContextCompat.getDrawable(RoomActivity.this,R.drawable.exit);
+        img2.setBounds(0, 0, 70, 70);
+        no.setCompoundDrawables(img2, null, null, null);
     }
 
-    public void removeGroup(){
-        groupRef.removeValue();
-        Log.d("remove test", "this is successed too");
+    public void showList(final RoomActivity.MyCallback myCallback) {
+        groupRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    if(ds.getKey().toString().equals("member")){
+                        Map<String, Object> membernames = (HashMap<String, Object>)ds.getValue();
+                        for(String membersKey : membernames.keySet()){
+                            memberList.put(membersKey, membernames.get(membersKey));
+                        }
+                    }
+                }
+                myCallback.onCallback(memberList);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+    }
+
+    public interface MyCallback {
+        void onCallback(Map<String, Object> memberList);
     }
 
 }
