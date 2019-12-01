82079904921fe4b81589edd49731e395f9ffb815
ovoHJ <s2018w01@gmail.com>
2019-12-01 23:00:37 +0900

    add!!!!!

--- app/src/main/java/kr/hs/emirim/dana/lookup/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/lookup/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,4 +35,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
+
 }
